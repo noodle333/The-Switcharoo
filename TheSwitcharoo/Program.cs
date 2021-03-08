7b097c8a60d60eb6a145ded045d163286ef59753
noodle333 <oliver.nordin@elev.ga.ntig.se>
2021-03-08 20:50:36 +0100

    first commit

---
+++ TheSwitcharoo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TheSwitcharoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a phrase!");
+            string x = Console.ReadLine();
+
+            Console.WriteLine("Your switcharoo phrase is : " + Switcheroo(x));
+            Console.ReadLine();
+        }
+
+        public static string Switcheroo(string x)
+        {
+            char[] charArray = x.ToCharArray();
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (charArray[i] == 'a')
+                {
+                    charArray[i] = 'b';
+                }
+                else if (charArray[i] == 'b')
+                {
+                    charArray[i] = 'a';
+                }
+            }
+            return new string(charArray);
+        }
+    }
+}
